99b917b0e679fae20a9bc6a80a59c3d0e9aa67a7
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2022-07-20 16:52:53 +0200

    typo

--- controllers/utilisateur.js
+++ controllers/utilisateur.js
@@ -2,19 +2,20 @@ const db = require('../models');
 const Utilisateur = db.utilisateur;
 const Op = db.Sequelize.Op;// Create 
 exports.create = (req, res) => {
-  const article = {
+  const utilisateur = {
     title: req.body.title,
     subtitle: req.body.subtitle,
     author: req.body.author,
     published: req.body.published ? req.body.published : false
-  };  Article.create(article)
+  };  
+  Utilisateur.create(utilisateur)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-        err.message || "Some error occurred while creating the Tutorial."
+        err.message || "Some error occurred while creating the User."
      });
     });
 };
@@ -27,13 +28,13 @@ exports.findOne = (req, res) => {
         res.send(data);
       } else {
         res.status(404).send({
-          message: `Cannot find Article with id=${id}.`
+          message: `Cannot find User with id=${id}.`
         });
       }
     })
     .catch(err => {
       res.status(500).send({
-      message: "Error retrieving Article with id=" + id
+      message: "Error retrieving User with id=" + id
     });
   });
 };
@@ -46,17 +47,17 @@ exports.update = (req, res) => {
   .then(num => {
     if (num == 1) {
       res.send({
-        message: "Article was updated successfully."
+        message: "User was updated successfully."
       });
     } else {
       res.send({
-        message: `Cannot update Article with id=${id}.`
+        message: `Cannot update User with id=${id}.`
       });
     }
   })
   .catch(err => {
     res.status(500).send({
-      message: "Error updating Article with id=" + id
+      message: "Error updating User with id=" + id
     });
   });
 };
@@ -69,17 +70,17 @@ exports.delete = (req, res) => {
   .then(num => {
     if (num == 1) {
       res.send({
-        message: "Article was deleted successfully!"
+        message: "User was deleted successfully!"
       });
     } else {
       res.send({
-        message: `Cannot delete Article with id=${id}.`
+        message: `Cannot delete User with id=${id}.`
       });
     }
   })
   .catch(err => {
     res.status(500).send({
-      message: "Could not delete Article with id=" + id
+      message: "Could not delete User with id=" + id
     });
   });
 };
